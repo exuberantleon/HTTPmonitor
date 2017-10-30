435b13365371457ba21e8a47048e359ee6355746
exuberantleon <exuberantleon@gmail.com>
2017-10-30 16:22:27 +1100

    Initial commit

---
+++ src/main/java/com/monitor/HTTPMonitor/HttpMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.HTTPMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpMonitorApplication.class, args);
+		
+	}
+}
